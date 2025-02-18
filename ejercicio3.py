15c979ec1839e408bc4d6b2196e63808024f8402
up240128 <up240128@alumnos.upa.edu.mx>
2025-02-18 08:46:51 -0600

    avance del trabajo

---
+++ ejercicio3.py
@@ -0,0 +1,6 @@
+#Program 4
+#Write a script that prompts the user to enter base and height
+#of the triangle and calculate an area of the triangle
+# (are = 0.5 x b x h)
+base = float(input('ingresa el valor de la base: '))
+altura = float(input('ingresa el valor de la altura: '))
