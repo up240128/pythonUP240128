24007900e29afc314ca71767c9cb2efb171bb2e8
up240128 <up240128@alumnos.upa.edu.mx>
2025-02-26 10:36:29 -0600

    avance del dia 5

---
+++ ejercicio_5.py
@@ -0,0 +1,35 @@
+#1 Declarar una lista vacía
+lst = list()
+print(len(lst))
+
+#2 Declarar una lista con más de 5 elementos
+lista = ['fresa', 'mango', 'pepino', 'uva', 'guayaba']
+print('Los elementos que tienen son: ', len(lista))
+print ('La lista de los elementos es la siguiente: ', (lista)) 
+
+#3 Encuentra la longitud de tu lista
+print('La longitud de la lista es: ', len(lista))
+
+#4 Obtener el primer elemento, el elemento del medio y el último elemento de la lista.
+fruits = ['fresa', 'mango', 'pepino', 'uva', 'guayaba']
+fruits[0] = 'fresa'       
+fruits[3] = 'pepino'      
+fruits[4] = 'guayaba' 
+print(fruits)    
+print('El siguiente orden son los elemntos principales, del centro y el ultimo: ', fruits[0], fruits[3], fruits[4])
+
+#5 Declara una lista llamada mixed_data_types, coloca tu(nombre, edad, altura, estado civil, dirección)
+mixed_data_types = ['Dariana', '18', '1.75', 'soltera', 'Calle de los gallos']
+print('La lista de mis datos son: ', mixed_data_types)
+
+#6 Declare una variable de lista llamada it_companies y asigne los valores iniciales Facebook, Google, Microsoft, Apple, IBM, Oracle y Amazon.
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+print ('Las compañias de it son: ', it_companies)
+
+#7 Imprima la lista usando print()
+print ('Las compañias de it son: ', it_companies)
+
+#8 Imprima el número de empresas en la lista
+print ('El numero de empresas que hay en la lista es: ', len(it_companies))
+
+
