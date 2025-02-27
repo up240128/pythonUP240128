758f56f1cf6ca8fdafa8eb2296f0260ec6fed786
up240128 <up240128@alumnos.upa.edu.mx>
2025-02-27 11:50:14 -0600

    avances hasta el problema 13

--- ejercicio_5.py
+++ ejercicio_5.py
@@ -16,7 +16,7 @@ fruits[0] = 'fresa'
 fruits[3] = 'pepino'      
 fruits[4] = 'guayaba' 
 print(fruits)    
-print('El siguiente orden son los elemntos principales, del centro y el ultimo: ', fruits[0], fruits[3], fruits[4])
+print('El siguiente orden son los elementos principales, del centro y el ultimo: ', fruits[0], fruits[3], fruits[4])
 
 #5 Declara una lista llamada mixed_data_types, coloca tu(nombre, edad, altura, estado civil, dirección)
 mixed_data_types = ['Dariana', '18', '1.75', 'soltera', 'Calle de los gallos']
@@ -32,4 +32,31 @@ print ('Las compañias de it son: ', it_companies)
 #8 Imprima el número de empresas en la lista
 print ('El numero de empresas que hay en la lista es: ', len(it_companies))
 
+#9 Imprima la primera, la segunda y la última empresa.
+empresas = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+empresas[0] = 'Facebook'       
+empresas[3] = 'Apple'      
+empresas[6] = 'Amazon' 
+print(empresas)    
+print('El siguiente orden son los elementos principales, del centro y el ultimo: ', empresas[0], empresas[3], empresas[6])
 
+#10 Imprima el listado después de modificar una de las empresas
+empresas = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+empresas [1] = 'Amazon'
+empresas [0] = 'Microsoft'
+print ('Las empresas modificadas son: ', empresas) 
+
+#11 Añadir una empresa de TI a it_companies
+empresas = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon', 'Samsung', 'Hp', 'Mercado libre']
+print ('Las empresas modificadas son: ', empresas) 
+
+#12 Insertar una empresa de TI en el medio de la lista de empresas
+empresas = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon', 'Samsung', 'Hp', 'Mercado libre', 'intel']
+empresas [5] = 'Hp'
+print ('Las empresas modificadas son: ', empresas) 
+
+#13 Cambie uno de los nombres de it_companies a mayúsculas (¡IBM excluido!)
+empresas = 'Facebook' + ' ' + ' ' + 'Google'  + ' ' + 'Microsoft' + ' '  + 'Apple' + ' ' +  'IBM' + ' ' +  'Oracle'  + ' ' +  'Amazon'
+print( empresas.upper())  
+
+#14
