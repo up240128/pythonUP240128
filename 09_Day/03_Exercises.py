748c57287ed0df2cc7ee84f9ff54a24afe4f252f
up240128 <up240128@alumnos.upa.edu.mx>
2025-03-20 11:43:15 -0600

    dia 9 terminado

--- 09_Day/03_Exercises.py
+++ 09_Day/03_Exercises.py
@@ -32,7 +32,6 @@ if 'skills' in person:
         print('Python esta en la lista de habilidades')
 
 #3 Si una persona tiene habilidades solo JavaScript y React, imprima ('Es un desarrollador front-end')
-#  imprima ('título desconocido'): para obtener resultados más precisos, se pueden anidar más condiciones.
 if 'skills' in person:
     if 'JavaScrip' and 'React' in person ['skills']:
         print('Es un desarrollador front-end')
@@ -42,9 +41,28 @@ if 'skills' in person:
     if 'node' or 'python' and 'MogoDB' in person ['skills']:
          print('Es un desarrollador backend')
 
-#5 si la persona tiene habilidades React, Node y MongoDB, Imprima ('Es un desarrollador completo'), de lo contrario
+#5 si la persona tiene habilidades React, Node y MongoDB, Imprima ('Es un desarrollador completo'), de lo contrario imprima ('título desconocido'): para obtener resultados más precisos, se pueden anidar más condiciones.
+if 'skills' in person:
+    if 'React' or 'Node' and 'MogoDB' in person ['skills']:
+         print('Es un desarrollador completo')
+    else:
+        print ('título desconocido')
 
-
+# 6 Si la persona está casada y vive en Finlandia, imprima la información en el siguiente formato:Asabeneh Yetayeh lives in Finland. He is married.
+persona1={
+    'first_name': 'Maria',
+    'last_name': '02',
+    'age': 18,
+    'country': 'Finland',
+    'is_marred': True,
+    'skills': ['Proteus', 'Python', 'JavaScript', 'Autocad','Fusion 360'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+    }
+if persona1['is_marred'] == True and persona1['country'] == 'Finland':
+    print("Asabeneh Yetayeh lives in Finland and He is married")
 
 
 
